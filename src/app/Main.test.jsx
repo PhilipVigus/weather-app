358f1d1832a74370314861c93dfb1f83955c8c96
PhilipVigus <philipvigus@yahoo.co.uk>
2020-08-10 16:10:01 +0100

    Implement app getting data from API
    The app is currently hard-coded to get the current weather for
    London. In the future, this will be configurable by the user.

--- src/app/Main.test.jsx
+++ src/app/Main.test.jsx
@@ -2,32 +2,172 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
+import Axios from "axios";
+import MockAdapter from "axios-mock-adapter";
 import Main from "./Main";
+import { getWeather } from "../features/weatherNow/weatherNowSlice";
+
+jest.mock("../features/weatherNow/weatherNowSlice");
 
 const mockStore = configureStore([]);
 
 describe("Main", () => {
-  it("renders Hello World", () => {
-    const store = mockStore({});
+  let mock;
 
-    render(
-      <Provider store={store}>
-        <Main />
-      </Provider>
-    );
-    expect(screen.getByText(/Hello World/)).toBeInTheDocument();
+  beforeAll(() => {
+    mock = new MockAdapter(Axios);
   });
 
-  it("renders the state value in the redux store", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  afterAll(() => {
+    mock.restore();
+  });
+
+  it("renders the heading", () => {
     const store = mockStore({
-      test: "This is a test"
+      test: "",
+      weatherNow: {
+        london: {
+          coord: { lon: -0.13, lat: 51.51 },
+          weather: [
+            { id: 801, main: "Clouds", description: "few clouds", icon: "02d" }
+          ],
+          base: "stations",
+          main: {
+            temp: 304.61,
+            feels_like: 305.31,
+            temp_min: 304.15,
+            temp_max: 305.93,
+            pressure: 1013,
+            humidity: 43
+          },
+          visibility: 10000,
+          wind: { speed: 2.6, deg: 80 },
+          clouds: { all: 13 },
+          dt: 1597063048,
+          sys: {
+            type: 1,
+            id: 1414,
+            country: "GB",
+            sunrise: 1597034324,
+            sunset: 1597087983
+          },
+          timezone: 3600,
+          id: 2643743,
+          name: "London",
+          cod: 200
+        }
+      }
     });
+
+    store.dispatch = jest.fn();
 
     render(
       <Provider store={store}>
         <Main />
       </Provider>
     );
-    expect(screen.getByText(/This is a test/)).toBeInTheDocument();
+    expect(screen.getByText(/Weather in London right now/)).toBeInTheDocument();
+  });
+
+  it("renders the state value in the redux store", async () => {
+    const store = mockStore({
+      test: "",
+      weatherNow: {
+        london: {
+          coord: { lon: -0.13, lat: 51.51 },
+          weather: [
+            { id: 801, main: "Clouds", description: "few clouds", icon: "02d" }
+          ],
+          base: "stations",
+          main: {
+            temp: 304.61,
+            feels_like: 305.31,
+            temp_min: 304.15,
+            temp_max: 305.93,
+            pressure: 1013,
+            humidity: 43
+          },
+          visibility: 10000,
+          wind: { speed: 2.6, deg: 80 },
+          clouds: { all: 13 },
+          dt: 1597063048,
+          sys: {
+            type: 1,
+            id: 1414,
+            country: "GB",
+            sunrise: 1597034324,
+            sunset: 1597087983
+          },
+          timezone: 3600,
+          id: 2643743,
+          name: "London",
+          cod: 200
+        }
+      }
+    });
+
+    store.dispatch = jest.fn();
+
+    render(
+      <Provider store={store}>
+        <Main />
+      </Provider>
+    );
+    expect(screen.getByText(/Humidity = 43/)).toBeInTheDocument();
+    expect(store.dispatch).toHaveBeenCalledTimes(1);
+    expect(getWeather).toHaveBeenCalledTimes(1);
+  });
+
+  it("calls dispatch on the redux store with the correct action", async () => {
+    const store = mockStore({
+      test: "",
+      weatherNow: {
+        london: {
+          coord: { lon: -0.13, lat: 51.51 },
+          weather: [
+            { id: 801, main: "Clouds", description: "few clouds", icon: "02d" }
+          ],
+          base: "stations",
+          main: {
+            temp: 304.61,
+            feels_like: 305.31,
+            temp_min: 304.15,
+            temp_max: 305.93,
+            pressure: 1013,
+            humidity: 43
+          },
+          visibility: 10000,
+          wind: { speed: 2.6, deg: 80 },
+          clouds: { all: 13 },
+          dt: 1597063048,
+          sys: {
+            type: 1,
+            id: 1414,
+            country: "GB",
+            sunrise: 1597034324,
+            sunset: 1597087983
+          },
+          timezone: 3600,
+          id: 2643743,
+          name: "London",
+          cod: 200
+        }
+      }
+    });
+
+    store.dispatch = jest.fn();
+
+    render(
+      <Provider store={store}>
+        <Main />
+      </Provider>
+    );
+
+    expect(store.dispatch).toHaveBeenCalledTimes(1);
+    expect(getWeather).toHaveBeenCalledTimes(1);
   });
 });
--- src/features/weatherNow/weatherNowSlice.js
+++ src/features/weatherNow/weatherNowSlice.js
@@ -1,10 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import Axios from "axios";
 
-const initialState = {};
+const initialState = { london: "" };
+
+export const getWeather = createAsyncThunk(
+  "weatherNow/getWeather",
+  async () => {
+    const result = await Axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?q=London&appid=${process.env.REACT_APP_API_KEY}`
+    );
+    return result.data;
+  }
+);
+
 const weatherNowSlice = createSlice({
   name: "weatherNow",
   initialState,
-  reducers: {}
+  reducers: {},
+  extraReducers: {
+    [getWeather.fulfilled]: (state, action) => {
+      state.london = action.payload;
+    }
+  }
 });
 
 export default weatherNowSlice.reducer;
