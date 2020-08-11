774353111a7f6c06471e83de63849a89957ad5d6
PhilipVigus <philipvigus@yahoo.co.uk>
2020-08-11 13:33:59 +0100

    Remove testSliceReducer from store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import testReducer from "../features/test/testSlice";
 import weatherNowReducer from "../features/weatherNow/weatherNowSlice";
 
 export default configureStore({
   reducer: {
-    test: testReducer,
     weatherNow: weatherNowReducer
   }
 });
