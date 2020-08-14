bf6771728ba4b4686cdccf1727e730b1f9ea48c7
PhilipVigus <philipvigus@yahoo.co.uk>
2020-08-14 14:22:51 +0100

    Reorganise tests to be lower level
    A lot of the testing was being done at App level, creating complex
    tests that were hard to maintain.
    
    The testing was moved down to individual component level, making
    them far easier to write and work with.

--- src/features/cityList/CityList.test.jsx
+++ src/features/cityList/CityList.test.jsx
@@ -81,4 +81,213 @@ describe("CityList", () => {
       "p"
     );
   });
+
+  it("clears the box when it gains focus", () => {
+    const store = mockStore({
+      cityList: {
+        cities: []
+      },
+      weatherNow: {
+        locations: [{ name: "London" }]
+      }
+    });
+
+    store.dispatch = jest.fn();
+
+    render(
+      <Provider store={store}>
+        <Router>
+          <CityList />
+        </Router>
+      </Provider>
+    );
+
+    const textbox = screen.getByPlaceholderText(/Enter city name/);
+    expect(textbox.value).toBe("London");
+    fireEvent.focus(textbox);
+    expect(textbox.value).toBe("");
+  });
+
+  it("shows the filtered list", () => {
+    const store = mockStore({
+      cityList: {
+        cities: [
+          { id: 1, name: "London" },
+          { id: 2, name: "Liverpool" },
+          { id: 3, name: "Lincoln" }
+        ]
+      },
+      weatherNow: {
+        locations: [{ name: "London" }]
+      }
+    });
+
+    store.dispatch = jest.fn();
+
+    render(
+      <Provider store={store}>
+        <Router>
+          <CityList />
+        </Router>
+      </Provider>
+    );
+
+    const textbox = screen.getByPlaceholderText(/Enter city name/);
+    fireEvent.focus(textbox);
+    fireEvent.change(textbox, { target: { value: "l" } });
+
+    expect(screen.getByText(/London/)).toBeInTheDocument();
+    expect(screen.getByText(/Liverpool/)).toBeInTheDocument();
+    expect(screen.getByText(/Lincoln/)).toBeInTheDocument();
+  });
+
+  it("shows the shortened filtered list", () => {
+    const store = mockStore({
+      cityList: {
+        cities: [
+          { id: 1, name: "London" },
+          { id: 2, name: "Liverpool" },
+          { id: 3, name: "Lincoln" },
+          { id: 4, name: "Liverpool" },
+          { id: 5, name: "Liverpool" },
+          { id: 6, name: "Liverpool" },
+          { id: 7, name: "Liverpool" },
+          { id: 8, name: "Liverpool" },
+          { id: 9, name: "Liverpool" },
+          { id: 10, name: "Liverpool" },
+          { id: 11, name: "Liverpool" },
+          { id: 12, name: "Liverpool" },
+          { id: 13, name: "Liverpool" },
+          { id: 14, name: "Liverpool" },
+          { id: 15, name: "Liverpool" },
+          { id: 16, name: "Liverpool" },
+          { id: 17, name: "Liverpool" },
+          { id: 18, name: "Liverpool" },
+          { id: 19, name: "Liverpool" },
+          { id: 20, name: "Liverpool" },
+          { id: 21, name: "Liverpool" },
+          { id: 22, name: "Liverpool" }
+        ]
+      },
+      weatherNow: {
+        locations: [{ name: "London" }]
+      }
+    });
+
+    store.dispatch = jest.fn();
+
+    render(
+      <Provider store={store}>
+        <Router>
+          <CityList />
+        </Router>
+      </Provider>
+    );
+
+    const textbox = screen.getByPlaceholderText(/Enter city name/);
+    fireEvent.focus(textbox);
+    fireEvent.change(textbox, { target: { value: "l" } });
+
+    expect(screen.getByText(/\+2 matches/)).toBeInTheDocument();
+  });
+
+  it("hides the filtered list when you click it", () => {
+    const store = mockStore({
+      cityList: {
+        cities: [
+          { id: 1, name: "London" },
+          { id: 2, name: "Liverpool" },
+          { id: 3, name: "Lincoln" }
+        ]
+      },
+      weatherNow: {
+        locations: [{ name: "London" }]
+      }
+    });
+
+    store.dispatch = jest.fn();
+
+    render(
+      <Provider store={store}>
+        <Router>
+          <CityList />
+        </Router>
+      </Provider>
+    );
+
+    const textbox = screen.getByPlaceholderText(/Enter city name/);
+    fireEvent.focus(textbox);
+    fireEvent.change(textbox, { target: { value: "l" } });
+    const london = screen.getByText(/London/);
+    fireEvent.click(london);
+
+    expect(screen.queryByText(/London/)).toBeNull();
+    expect(screen.queryByText(/Liverpool/)).toBeNull();
+    expect(screen.queryByText(/Lincoln/)).toBeNull();
+  });
+
+  it("hides the filtered list when you press enter", () => {
+    const store = mockStore({
+      cityList: {
+        cities: [
+          { id: 1, name: "London" },
+          { id: 2, name: "Liverpool" },
+          { id: 3, name: "Lincoln" }
+        ]
+      },
+      weatherNow: {
+        locations: [{ name: "London" }]
+      }
+    });
+
+    store.dispatch = jest.fn();
+
+    render(
+      <Provider store={store}>
+        <Router>
+          <CityList />
+        </Router>
+      </Provider>
+    );
+
+    const textbox = screen.getByPlaceholderText(/Enter city name/);
+    fireEvent.focus(textbox);
+    fireEvent.change(textbox, { target: { value: "l" } });
+    fireEvent.keyDown(textbox, { key: "Enter", code: "Enter" });
+
+    expect(screen.queryByText(/London/)).toBeNull();
+    expect(screen.queryByText(/Liverpool/)).toBeNull();
+    expect(screen.queryByText(/Lincoln/)).toBeNull();
+  });
+
+  it("shows no matches if there are no filtered cities", () => {
+    const store = mockStore({
+      cityList: {
+        cities: [
+          { id: 1, name: "London" },
+          { id: 2, name: "Liverpool" },
+          { id: 3, name: "Lincoln" }
+        ]
+      },
+      weatherNow: {
+        locations: [{ name: "London" }]
+      }
+    });
+
+    store.dispatch = jest.fn();
+
+    render(
+      <Provider store={store}>
+        <Router>
+          <CityList />
+        </Router>
+      </Provider>
+    );
+
+    const textbox = screen.getByPlaceholderText(/Enter city name/);
+    fireEvent.focus(textbox);
+    fireEvent.change(textbox, { target: { value: "p" } });
+
+    expect(screen.getByText(/No matches/)).toBeInTheDocument();
+  });
 });
--- src/features/cityList/FilteredCity.test.jsx
+++ src/features/cityList/FilteredCity.test.jsx
@@ -5,14 +5,14 @@ import FilteredCity from "./FilteredCity";
 describe("FilteredCity", () => {
   it("renders the name", () => {
     const callback = jest.fn();
-    render(<FilteredCity name="London" id="133" callback={callback} />);
+    render(<FilteredCity name="London" id={133} callback={callback} />);
 
     expect(screen.getByText(/London/)).toBeInTheDocument();
   });
 
   it("calls the callback when you click on it", () => {
     const callback = jest.fn();
-    render(<FilteredCity name="London" id="133" callback={callback} />);
+    render(<FilteredCity name="London" id={133} callback={callback} />);
     const city = screen.getByText(/London/);
     fireEvent.click(city);
 
