7a8ee05aac31aa2154d6217a0fe0799e74164083
PhilipVigus <philipvigus@yahoo.co.uk>
2020-08-14 08:25:40 +0100

    Add react-router-dom

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 import Main from "./Main";
 import store from "./store";
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Main />
+      <Router>
+        <Main />
+      </Router>
     </Provider>
   );
 };
--- src/app/Main.jsx
+++ src/app/Main.jsx
@@ -1,13 +1,36 @@
 import React from "react";
+import { Switch, Route, Redirect, Link } from "react-router-dom";
 import WeatherNow from "../features/weatherNow/WeatherNow";
 import CityList from "../features/cityList/CityList";
 
 const Main = () => {
   return (
-    <>
-      <CityList />
-      <WeatherNow />
-    </>
+    <Switch>
+      <Route
+        exact
+        path="/"
+        render={() => {
+          return (
+            <>
+              <CityList />
+              <WeatherNow />
+            </>
+          );
+        }}
+      />
+      <Route
+        exact
+        path="/:id"
+        render={() => {
+          return (
+            <>
+              <CityList />
+              <WeatherNow />
+            </>
+          );
+        }}
+      />
+    </Switch>
   );
 };
 
