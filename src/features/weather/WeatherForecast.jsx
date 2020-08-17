43cf2b8009cba6e9367a82af4405d2131457ec16
PhilipVigus <philipvigus@yahoo.co.uk>
2020-08-17 13:51:26 +0100

    Show loading message in WeatherForecast

--- src/features/weather/WeatherForecast.jsx
+++ src/features/weather/WeatherForecast.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const WeatherForecast = () => {
+  const weatherForecast = useSelector((state) => state.weather.forecast);
+
+  if (!weatherForecast) {
+    return <div>Loading...</div>;
+  }
   return <div>Forecast</div>;
 };
 
--- src/features/weather/WeatherForecast.test.jsx
+++ src/features/weather/WeatherForecast.test.jsx
@@ -1,11 +1,45 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
 import WeatherForecast from "./WeatherForecast";
+
+const mockStore = configureStore([]);
 
 describe("WeatherForecast", () => {
   it("Renders the title", () => {
-    render(<WeatherForecast />);
+    const store = mockStore({
+      locationList: {
+        locations: []
+      },
+      weather: {
+        forecast: {}
+      }
+    });
+
+    render(
+      <Provider store={store}>
+        <WeatherForecast />
+      </Provider>
+    );
 
     expect(screen.getByText(/Forecast/)).toBeInTheDocument();
+  });
+
+  it("Renders the loading message", () => {
+    const store = mockStore({
+      locationList: {
+        locations: []
+      },
+      weather: {}
+    });
+
+    render(
+      <Provider store={store}>
+        <WeatherForecast />
+      </Provider>
+    );
+
+    expect(screen.getByText(/Loading.../)).toBeInTheDocument();
   });
 });
