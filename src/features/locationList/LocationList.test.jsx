e5023e1890750a2322bec32ff6fec9481ddb8f92
PhilipVigus <philipvigus@yahoo.co.uk>
2020-08-19 09:42:10 +0100

    Refactor LocationList tests

--- src/features/locationList/LocationList.test.jsx
+++ src/features/locationList/LocationList.test.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
-import Axios from "axios";
-import MockAdapter from "axios-mock-adapter";
 import { MemoryRouter as Router } from "react-router-dom";
 import LocationList from "./LocationList";
 import * as locationListSlice from "./locationListSlice";
 import * as weatherSlice from "../weather/weatherSlice";
+import londonWeatherNow from "../../fixtures/londonWeatherNow";
+import londonWeatherForecast from "../../fixtures/londonWeatherForecast";
+import locatioinsWithInitialLetterL from "../../fixtures/locationsWithInitialL";
 
 jest.mock("./locationListSlice");
 jest.mock("../weather/weatherSlice");
@@ -15,27 +16,18 @@ jest.mock("../weather/weatherSlice");
 const mockStore = configureStore([]);
 
 describe("LocationList", () => {
-  let mock;
-
-  beforeAll(() => {
-    mock = new MockAdapter(Axios);
-  });
-
   afterEach(() => {
     jest.clearAllMocks();
-  });
-
-  afterAll(() => {
-    mock.restore();
   });
 
   it("renders the title", () => {
     const store = mockStore({
       locationList: {
+        cachedLetters: {},
         locations: []
       },
       weather: {
-        weather: {}
+        GPSAvailable: true
       }
     });
 
@@ -51,17 +43,21 @@ describe("LocationList", () => {
     expect(screen.getByText(/Location List/)).toBeInTheDocument();
   });
 
-  it("dispatchs fetchLocationsWithInitialLetter action when you press return", () => {
+  it("dispatchs a fetchLocationsWithInitialLetter action when you press return", () => {
     const store = mockStore({
       locationList: {
+        cachedLetters: {},
         locations: []
       },
       weather: {
-        weather: {}
+        weather: {
+          GPSAvailable: true
+        }
       }
     });
 
     store.dispatch = jest.fn();
+
     render(
       <Provider store={store}>
         <Router>
@@ -84,13 +80,16 @@ describe("LocationList", () => {
     ).toHaveBeenCalledWith("p");
   });
 
-  it("clears the box when it gains focus", () => {
+  it("clears the textbox when it gains focus", () => {
     const store = mockStore({
       locationList: {
-        locations: []
+        cachedLetters: {},
+        locations: locatioinsWithInitialLetterL
       },
       weather: {
-        now: { name: "London" }
+        now: londonWeatherNow,
+        forecast: londonWeatherForecast,
+        GPSAvailable: true
       }
     });
 
@@ -106,21 +105,21 @@ describe("LocationList", () => {
 
     const textbox = screen.getByPlaceholderText(/Enter location name/);
     expect(textbox.value).toBe("London");
+
     fireEvent.focus(textbox);
     expect(textbox.value).toBe("");
   });
 
-  it("shows the filtered list", () => {
+  it("shows the full filtered list when there are 20 or fewer items on the list", () => {
     const store = mockStore({
       locationList: {
-        locations: [
-          { id: 1, name: "London" },
-          { id: 2, name: "Liverpool" },
-          { id: 3, name: "Lincoln" }
-        ]
+        cachedLetters: {},
+        locations: locatioinsWithInitialLetterL.slice(0, 20)
       },
       weather: {
-        weather: { now: { name: "London" } }
+        now: londonWeatherNow,
+        forecast: londonWeatherForecast,
+        GPSAvailable: true
       }
     });
 
@@ -138,41 +137,25 @@ describe("LocationList", () => {
     fireEvent.focus(textbox);
     fireEvent.change(textbox, { target: { value: "l" } });
 
-    expect(screen.getByText(/London/)).toBeInTheDocument();
-    expect(screen.getByText(/Liverpool/)).toBeInTheDocument();
-    expect(screen.getByText(/Lincoln/)).toBeInTheDocument();
+    expect(
+      screen.getByText(/L'Abbaye, Switzerland \(6\.30°, 46\.64°\)/)
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(/L'Aulnois, Belgium \(4\.34°, 50\.51°\)/)
+    ).toBeInTheDocument();
+    expect(screen.queryByText(/matches/)).toBeNull();
   });
 
-  it("shows the shortened filtered list", () => {
+  it("shows the shortened filtered list with a summary of excess matches not shown", () => {
     const store = mockStore({
       locationList: {
-        locations: [
-          { id: 1, name: "London" },
-          { id: 2, name: "Liverpool" },
-          { id: 3, name: "Lincoln" },
-          { id: 4, name: "Liverpool" },
-          { id: 5, name: "Liverpool" },
-          { id: 6, name: "Liverpool" },
-          { id: 7, name: "Liverpool" },
-          { id: 8, name: "Liverpool" },
-          { id: 9, name: "Liverpool" },
-          { id: 10, name: "Liverpool" },
-          { id: 11, name: "Liverpool" },
-          { id: 12, name: "Liverpool" },
-          { id: 13, name: "Liverpool" },
-          { id: 14, name: "Liverpool" },
-          { id: 15, name: "Liverpool" },
-          { id: 16, name: "Liverpool" },
-          { id: 17, name: "Liverpool" },
-          { id: 18, name: "Liverpool" },
-          { id: 19, name: "Liverpool" },
-          { id: 20, name: "Liverpool" },
-          { id: 21, name: "Liverpool" },
-          { id: 22, name: "Liverpool" }
-        ]
+        cachedLetters: {},
+        locations: locatioinsWithInitialLetterL
       },
       weather: {
-        weather: { now: { name: "London" } }
+        now: londonWeatherNow,
+        forecast: londonWeatherForecast,
+        GPSAvailable: true
       }
     });
 
@@ -189,6 +172,13 @@ describe("LocationList", () => {
     const textbox = screen.getByPlaceholderText(/Enter location name/);
     fireEvent.focus(textbox);
     fireEvent.change(textbox, { target: { value: "l" } });
+
+    expect(
+      screen.getByText(/L'Aulnois, Belgium \(4\.34°, 50\.51°\)/)
+    ).toBeInTheDocument();
+    expect(
+      screen.queryByText(/L'Aunay, France \(0\.38°, 48\.73°\)/)
+    ).toBeNull();
 
     expect(screen.getByText(/\+2 matches/)).toBeInTheDocument();
   });
@@ -196,14 +186,13 @@ describe("LocationList", () => {
   it("hides the filtered list when you click it", () => {
     const store = mockStore({
       locationList: {
-        locations: [
-          { id: 1, name: "London" },
-          { id: 2, name: "Liverpool" },
-          { id: 3, name: "Lincoln" }
-        ]
+        cachedLetters: {},
+        locations: locatioinsWithInitialLetterL
       },
       weather: {
-        weather: { now: { name: "London" } }
+        now: londonWeatherNow,
+        forecast: londonWeatherForecast,
+        GPSAvailable: true
       }
     });
 
@@ -220,25 +209,26 @@ describe("LocationList", () => {
     const textbox = screen.getByPlaceholderText(/Enter location name/);
     fireEvent.focus(textbox);
     fireEvent.change(textbox, { target: { value: "l" } });
-    const london = screen.getByText(/London/);
-    fireEvent.click(london);
+    const filteredLocation = screen.getByText(
+      /L'Ametlla del Vallès, Spain \(2\.27°, 41\.67°\)/
+    );
+    fireEvent.click(filteredLocation);
 
-    expect(screen.queryByText(/London/)).toBeNull();
-    expect(screen.queryByText(/Liverpool/)).toBeNull();
-    expect(screen.queryByText(/Lincoln/)).toBeNull();
+    expect(
+      screen.queryByText(/L'Ametlla del Vallès, Spain \(2\.27°, 41\.67°\)/)
+    ).toBeNull();
   });
 
   it("hides the filtered list when you press enter", () => {
     const store = mockStore({
       locationList: {
-        locations: [
-          { id: 1, name: "London" },
-          { id: 2, name: "Liverpool" },
-          { id: 3, name: "Lincoln" }
-        ]
+        cachedLetters: {},
+        locations: locatioinsWithInitialLetterL
       },
       weather: {
-        weather: { now: { name: "London" } }
+        now: londonWeatherNow,
+        forecast: londonWeatherForecast,
+        GPSAvailable: true
       }
     });
 
@@ -257,22 +247,21 @@ describe("LocationList", () => {
     fireEvent.change(textbox, { target: { value: "l" } });
     fireEvent.keyDown(textbox, { key: "Enter", code: "Enter" });
 
-    expect(screen.queryByText(/London/)).toBeNull();
-    expect(screen.queryByText(/Liverpool/)).toBeNull();
-    expect(screen.queryByText(/Lincoln/)).toBeNull();
+    expect(
+      screen.queryByText(/L'Ametlla del Vallès, Spain \(2\.27°, 41\.67°\)/)
+    ).toBeNull();
   });
 
   it("shows no matches if there are no filtered locations", () => {
     const store = mockStore({
       locationList: {
-        locations: [
-          { id: 1, name: "London" },
-          { id: 2, name: "Liverpool" },
-          { id: 3, name: "Lincoln" }
-        ]
+        cachedLetters: {},
+        locations: locatioinsWithInitialLetterL
       },
       weather: {
-        now: { name: "London" }
+        now: londonWeatherNow,
+        forecast: londonWeatherForecast,
+        GPSAvailable: true
       }
     });
 
@@ -296,14 +285,13 @@ describe("LocationList", () => {
   it("shows the button for setting the location to GPS", () => {
     const store = mockStore({
       locationList: {
-        locations: [
-          { id: 1, name: "London" },
-          { id: 2, name: "Liverpool" },
-          { id: 3, name: "Lincoln" }
-        ]
+        cachedLetters: {},
+        locations: locatioinsWithInitialLetterL
       },
       weather: {
-        now: { name: "London" }
+        now: londonWeatherNow,
+        forecast: londonWeatherForecast,
+        GPSAvailable: true
       }
     });
 
@@ -322,7 +310,7 @@ describe("LocationList", () => {
     ).toBeInTheDocument();
   });
 
-  it("the GPS button calls dispatch", async () => {
+  it("the GPS button calls the getWeatherByGPS action", async () => {
     const mockGeolocation = {
       getCurrentPosition: jest.fn().mockImplementationOnce((success) =>
         Promise.resolve(
@@ -337,16 +325,15 @@ describe("LocationList", () => {
     };
 
     global.navigator.geolocation = mockGeolocation;
+
     const store = mockStore({
       locationList: {
-        locations: [
-          { id: 1, name: "London" },
-          { id: 2, name: "Liverpool" },
-          { id: 3, name: "Lincoln" }
-        ]
+        cachedLetters: {},
+        locations: locatioinsWithInitialLetterL
       },
       weather: {
-        now: { name: "London" },
+        now: londonWeatherNow,
+        forecast: londonWeatherForecast,
         GPSAvailable: true
       }
     });
@@ -363,21 +350,19 @@ describe("LocationList", () => {
 
     const button = screen.getByRole("button", { name: "Where I am" });
     fireEvent.click(button);
-    expect(store.dispatch).toHaveBeenCalledTimes(2);
+
     expect(weatherSlice.getWeatherByGPS).toHaveBeenCalledTimes(1);
   });
 
   it("the GPS button is disabled when GPS is unavailable", async () => {
     const store = mockStore({
       locationList: {
-        locations: [
-          { id: 1, name: "London" },
-          { id: 2, name: "Liverpool" },
-          { id: 3, name: "Lincoln" }
-        ]
+        cachedLetters: {},
+        locations: locatioinsWithInitialLetterL
       },
       weather: {
-        now: { name: "London" },
+        now: londonWeatherNow,
+        forecast: londonWeatherForecast,
         GPSAvailable: false
       }
     });
@@ -394,7 +379,7 @@ describe("LocationList", () => {
 
     const button = screen.getByRole("button", { name: "Where I am" });
     fireEvent.click(button);
-    expect(store.dispatch).toHaveBeenCalledTimes(1);
+
     expect(weatherSlice.getWeatherByGPS).toHaveBeenCalledTimes(0);
   });
 });
