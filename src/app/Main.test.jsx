6cab4d4f25ad6852ba0e2789870cdd2e60f0a5d6
PhilipVigus <philipvigus@yahoo.co.uk>
2020-08-19 08:52:52 +0100

    Refactor Main tests

--- src/app/Main.test.jsx
+++ src/app/Main.test.jsx
@@ -4,60 +4,34 @@ import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
 import { MemoryRouter as Router } from "react-router-dom";
 import Main from "./Main";
+import londonWeatherNow from "../fixtures/londonWeatherNow";
+import londonWeatherForecast from "../fixtures/londonWeatherForecast";
+import locationsWithInitialL from "../fixtures/locationsWithInitialL";
 
 const mockStore = configureStore([]);
 
 describe("Main", () => {
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+  let store;
 
-  it("renders the WeatherNow component", () => {
-    const store = mockStore({
+  beforeAll(() => {
+    store = mockStore({
       locationList: {
-        locations: []
+        locations: locationsWithInitialL
       },
       weather: {
-        now: {
-          coord: { lon: -0.13, lat: 51.51 },
-          weather: [
-            {
-              id: 801,
-              main: "Clouds",
-              description: "few clouds",
-              icon: "02d"
-            }
-          ],
-          base: "stations",
-          main: {
-            temp: 304.61,
-            feels_like: 305.31,
-            temp_min: 304.15,
-            temp_max: 305.93,
-            pressure: 1013,
-            humidity: 43
-          },
-          visibility: 10000,
-          wind: { speed: 2.6, deg: 80 },
-          clouds: { all: 13 },
-          dt: 1597063048,
-          sys: {
-            type: 1,
-            id: 1414,
-            country: "GB",
-            sunrise: 1597034324,
-            sunset: 1597087983
-          },
-          timezone: 3600,
-          id: 2643743,
-          name: "London",
-          cod: 200
-        }
+        now: londonWeatherNow,
+        forecast: londonWeatherForecast
       }
     });
 
     store.dispatch = jest.fn();
+  });
 
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
+  it("renders the WeatherNow component", () => {
     render(
       <Provider store={store}>
         <Router>
@@ -69,51 +43,6 @@ describe("Main", () => {
   });
 
   it("renders the LocationList component", () => {
-    const store = mockStore({
-      locationList: {
-        locations: []
-      },
-      weather: {
-        now: {
-          coord: { lon: -0.13, lat: 51.51 },
-          weather: [
-            {
-              id: 801,
-              main: "Clouds",
-              description: "few clouds",
-              icon: "02d"
-            }
-          ],
-          base: "stations",
-          main: {
-            temp: 304.61,
-            feels_like: 305.31,
-            temp_min: 304.15,
-            temp_max: 305.93,
-            pressure: 1013,
-            humidity: 43
-          },
-          visibility: 10000,
-          wind: { speed: 2.6, deg: 80 },
-          clouds: { all: 13 },
-          dt: 1597063048,
-          sys: {
-            type: 1,
-            id: 1414,
-            country: "GB",
-            sunrise: 1597034324,
-            sunset: 1597087983
-          },
-          timezone: 3600,
-          id: 2643743,
-          name: "London",
-          cod: 200
-        }
-      }
-    });
-
-    store.dispatch = jest.fn();
-
     render(
       <Provider store={store}>
         <Router>
