d745e1ba44de4a31e80ebe7b69294cc34f6df714
PhilipVigus <philipvigus@yahoo.co.uk>
2020-08-19 10:07:10 +0100

    Refactor WeatherContainer tests

--- src/features/weather/WeatherContainer.test.jsx
+++ src/features/weather/WeatherContainer.test.jsx
@@ -11,47 +11,14 @@ jest.mock("./weatherSlice");
 const mockStore = configureStore([]);
 
 describe("WeatherContainer", () => {
-  it("calls dispatch on the redux store with the correct action", async () => {
+  it("calls dispatch on the redux store with the getWeatherById action", async () => {
     const store = mockStore({
       locationList: {
+        cachedLetters: {},
         locations: []
       },
       weather: {
-        now: {
-          coord: { lon: -0.13, lat: 51.51 },
-          weather: [
-            {
-              id: 801,
-              main: "Clouds",
-              description: "few clouds",
-              icon: "02d"
-            }
-          ],
-          base: "stations",
-          main: {
-            temp: 304.61,
-            feels_like: 305.31,
-            temp_min: 304.15,
-            temp_max: 305.93,
-            pressure: 1013,
-            humidity: 43
-          },
-          visibility: 10000,
-          wind: { speed: 2.6, deg: 80 },
-          clouds: { all: 13 },
-          dt: 1597063048,
-          sys: {
-            type: 1,
-            id: 1414,
-            country: "GB",
-            sunrise: 1597034324,
-            sunset: 1597087983
-          },
-          timezone: 3600,
-          id: 2643743,
-          name: "location",
-          cod: 200
-        }
+        GPSAvailable: true
       }
     });
 
@@ -67,7 +34,6 @@ describe("WeatherContainer", () => {
       </Provider>
     );
 
-    expect(store.dispatch).toHaveBeenCalledTimes(1);
     expect(weatherNowSlice.getWeatherById).toHaveBeenCalledTimes(1);
     expect(weatherNowSlice.getWeatherById).toHaveBeenCalledWith("15");
   });
