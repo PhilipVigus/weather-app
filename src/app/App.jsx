8d87f51d8b7d29a26387e75a3131ac1e7e030999
PhilipVigus <philipvigus@yahoo.co.uk>
2020-08-19 11:09:58 +0100

    Add basic global style defaults

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,10 +3,12 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import Main from "./Main";
 import store from "./store";
+import GlobalStyle from "./GlobalStyle";
 
 const App = () => {
   return (
     <Provider store={store}>
+      <GlobalStyle />
       <Router>
         <Main />
       </Router>
