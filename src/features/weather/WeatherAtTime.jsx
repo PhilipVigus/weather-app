b8cab293fbe168b97a9a60f2fe24e242eb8dc36a
PhilipVigus <philipvigus@yahoo.co.uk>
2020-08-24 14:27:10 +0100

    Style WeatherAtTime

--- src/features/weather/WeatherAtTime.jsx
+++ src/features/weather/WeatherAtTime.jsx
@@ -2,26 +2,80 @@
 import React from "react";
 import PropTypes, { string, number, arrayOf } from "prop-types";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWind } from "@fortawesome/free-solid-svg-icons";
 
 const StyledWeatherAtTime = styled.div`
-  border: 1px solid black;
+  border-top: 1px solid grey;
+  border-bottom: 1px solid grey;
+  padding: 0 20px;
+`;
+
+const Time = styled.div`
+  border-bottom: 1px solid rgb(220, 220, 220);
+  margin: 0 10px;
+  padding: 5px 0;
+  text-align: center;
+`;
+
+const ImageContainer = styled.div`
+  text-align: center;
+`;
+
+const Temperature = styled.div`
+  font-size: 2rem;
+  font-weight: bold;
+  margin-top: -10px;
+  text-align: center;
+`;
+
+const Wind = styled.div`
+  text-align: center;
+`;
+
+const WindIcon = styled.div`
+  margin-top: 20px;
+  font-size: 2rem;
+`;
+
+const WindSpeedAndDirection = styled.div`
+  font-size: 1.6rem;
+`;
+
+const CloudCoverage = styled.div`
+  margin-top: 20px;
+  text-align: center;
+`;
+
+const Humidity = styled.div`
+  text-align: center;
 `;
 
 const WeatherAtTime = ({ forecast }) => {
   return (
     <StyledWeatherAtTime>
-      <div>{forecast.time}</div>
-      <div>{forecast.forecast.weather[0].main}</div>
-      <div>
+      <Time>{forecast.time}</Time>
+      <ImageContainer>
         <img
           alt={`${forecast.forecast.weather[0].main} icon`}
           src={`https://openweathermap.org/img/wn/${forecast.forecast.weather[0].icon}@2x.png`}
         />
-      </div>
-      <div>{Math.round(forecast.forecast.main.temp - 273.15)} C</div>
-      <div>{forecast.forecast.main.humidity}% humidity</div>
-      <div>{`Wind = ${forecast.forecast.wind.speed} m/s (${forecast.forecast.wind.deg} degrees)`}</div>
-      <div>{`${forecast.forecast.clouds.all}% cloud coverage`}</div>
+      </ImageContainer>
+      <Temperature>
+        {Math.round(forecast.forecast.main.temp - 273.15)}°
+      </Temperature>
+      <Wind>
+        <WindIcon>
+          <FontAwesomeIcon icon={faWind} />
+        </WindIcon>
+        <WindSpeedAndDirection>
+          {`${Math.round(forecast.forecast.wind.speed)} m/s (${
+            forecast.forecast.wind.deg
+          } °)`}
+        </WindSpeedAndDirection>
+      </Wind>
+      <CloudCoverage>{`${forecast.forecast.clouds.all}% cloud coverage`}</CloudCoverage>
+      <Humidity>{forecast.forecast.main.humidity}% humidity</Humidity>
     </StyledWeatherAtTime>
   );
 };
