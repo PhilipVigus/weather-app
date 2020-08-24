c01e3dd877bb9c60b128487591548a29930e3b72
PhilipVigus <philipvigus@yahoo.co.uk>
2020-08-24 13:42:17 +0100

    Refactor DayLink

--- src/features/weather/DayLink.jsx
+++ src/features/weather/DayLink.jsx
@@ -1,34 +1,17 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
 const StyledLink = styled.input`
-  background: white;
+  background: ${(props) => (props.focused ? " rgb(240, 240, 240)" : "white")};
   border: 1px solid grey;
   border-bottom: none;
   border-radius: 5px 5px 0 0;
   cursor: pointer;
-  font-size: 1.6rem;
-  padding: 15px;
+  font-size: ${(props) => (props.focused ? "1.8rem" : "1.6rem")};
+  padding: ${(props) => (props.focused ? "18px" : "15px")};
   margin: 0 5px;
   outline: none;
-
-  &:last-of-type {
-    border-right: 1px solid grey;
-  }
-`;
-
-const StyledLinkWithFocus = styled.input`
-  background: rgb(240, 240, 240);
-  border: 1px solid grey;
-  border-bottom: none;
-  border-radius: 5px 5px 0 0;
-  cursor: pointer;
-  font-size: 1.8rem;
-  font-weight: bold;
-  outline: none;
-  padding: 18px;
-  margin: 0 5px;
 
   &:last-of-type {
     border-right: 1px solid grey;
@@ -36,33 +19,14 @@ const StyledLinkWithFocus = styled.input`
 `;
 
 const DayLink = ({ day, index, clickCallback, hasFocus }) => {
-  const buttonRef = useRef(null);
-
-  useEffect(() => {
-    if (hasFocus) {
-      buttonRef.current.focus();
-    }
-  }, [hasFocus]);
-
-  if (hasFocus) {
-    return (
-      <StyledLinkWithFocus
-        ref={buttonRef}
-        type="button"
-        value={day}
-        onClick={() => clickCallback(index)}
-      />
-    );
-  } else {
-    return (
-      <StyledLink
-        ref={buttonRef}
-        type="button"
-        value={day}
-        onClick={() => clickCallback(index)}
-      />
-    );
-  }
+  return (
+    <StyledLink
+      focused={hasFocus}
+      type="button"
+      value={day}
+      onClick={() => clickCallback(index)}
+    />
+  );
 };
 
 DayLink.propTypes = {
